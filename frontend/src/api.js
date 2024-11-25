dc12f08f43a6ec0aa003a85da99e36489f8b5bed
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2024-11-25 10:22:07 +0530

    quiz-app

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://trivia-trail.vercel.app/api" });
 
 // Add Authorization header
 API.interceptors.request.use((req) => {
